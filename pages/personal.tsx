3d4ed9d04c2c6f6ca075201dbef6f101eb09e2aa
Mahesh Makani <maheshmakani@gmail.com>
2020-12-09 16:42:10 +0000

    add pages, custom document

---
+++ pages/personal.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/layout';
+
+export default function Personal() {
+  return (
+    <Layout>
+      <h3>Personal Experience</h3>
+    </Layout>
+  );
+}
---
+++ pages/work.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/layout';
+
+export default function Work() {
+  return (
+    <Layout>
+      <h3>Work Experience</h3>
+    </Layout>
+  );
+}
