4cd61c9fd52912e4fa0a93ccd5a681fb19d6daff
Mahesh Makani <maheshmakani@gmail.com>
2020-12-09 16:48:05 +0000

    add dates

--- pages/personal.tsx
+++ pages/personal.tsx
@@ -4,6 +4,8 @@ export default function Personal() {
   return (
     <Layout>
       <h3>Personal Experience</h3>
+      <h4>Software Developer | Fight Lab Brighton | Nov 2017 - Present</h4>
+      <h4>App Developer | Bracketor for Challonge | Dec 2015 - Mar 2016</h4>
     </Layout>
   );
 }
--- pages/work.tsx
+++ pages/work.tsx
@@ -4,6 +4,8 @@ export default function Work() {
   return (
     <Layout>
       <h3>Work Experience</h3>
+      <h4>Full Stack Software Engineer | The Guardian | Sep 2019 - Present</h4>
+      <h4>Software Developer | Net Natives | Sep 2015 - Sep 2019</h4>
     </Layout>
   );
 }
