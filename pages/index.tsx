057665e22e2b390e45fa0e2d286bed7372b12e82
Mahesh Makani <mahesh@makani.dev>
2022-07-11 20:59:13 +0100

    fix: fix linting errors after upgrading to next 12

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Layout from '../components/layout';
 
 export default function Home() {
@@ -7,13 +8,13 @@ export default function Home() {
       <p>Coding along the full stack across the way 👨‍💻</p>
       <p>Based in Hertfordshire and London, UK</p>
       <p>He/Him</p>
-      <img
-        src="mahesh.jpg"
+      <Image
+        src="/mahesh.jpg"
         alt="Mahesh Makani | Photo"
         className="avatar"
-        width="100"
-        height="100"
-      ></img>
+        width={100}
+        height={100}
+      ></Image>
     </Layout>
   );
 }
