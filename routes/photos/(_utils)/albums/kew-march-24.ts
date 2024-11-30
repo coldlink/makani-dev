8eadb5f1af6b3abb37772ee2f3462dfef50fba81
Mahesh Makani <mahesh@makani.dev>
2024-11-30 20:48:58 +0000

    fix(photo): update kew licenses

--- routes/photos/(_utils)/albums/kew-march-24.ts
+++ routes/photos/(_utils)/albums/kew-march-24.ts
@@ -21,13 +21,11 @@ export const KewMarch24: Album = {
 			src: "20240322-_87A9273-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-2",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9294-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-3",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9404-Enhanced-NR.jpg",
@@ -39,43 +37,36 @@ export const KewMarch24: Album = {
 			src: "20240322-_87A9204-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-5",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9279-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-6",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9301-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-7",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9427-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-8",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9260-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-9",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9284-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-10",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9319-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-11",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9438-Enhanced-NR.jpg",
@@ -87,19 +78,16 @@ export const KewMarch24: Album = {
 			src: "20240322-_87A9264-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-13",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9292-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-14",
-			noLicense: true,
 		},
 		{
 			src: "20240322-_87A9369-Enhanced-NR.jpg",
 			timezone: "Europe/London",
 			slug: "kew-gardens-15",
-			noLicense: true,
 		},
 	],
 };
