272adfecce9501938c2d5ed2efb5464df68a7024
Mahesh Makani <mahesh@makani.dev>
2024-11-30 10:34:40 +0000

    feat(photos): add tokyo day

--- routes/photos/(_utils)/albums.ts
+++ routes/photos/(_utils)/albums.ts
@@ -254,6 +254,64 @@ const Kyoto: Album = {
 	],
 };
 
+const TokyoDay: Album = {
+	name: "Tokyo Day Vibes",
+	slug: "tokyo-day-vibes",
+	dates: "April 2024",
+	copyrightYear: "2024",
+	photos: [
+		{
+			src: "20240420-3_87A1612-Enhanced-NR.jpg",
+			timezone: "Asia/Tokyo",
+			slug: "tokyo-station-sketch",
+			noLicense: true,
+		},
+		{
+			src: "20240425-3_87A2833-Enhanced-NR.jpg",
+			timezone: "Asia/Tokyo",
+			slug: "tokyo-construction",
+		},
+		{
+			src: "20240418-3_87A1508-Enhanced-NR.jpg",
+			timezone: "Asia/Tokyo",
+			slug: "tokyo-disney-teens",
+			noLicense: true,
+		},
+		{
+			src: "20240425-3_87A2760-Enhanced-NR.jpg",
+			timezone: "Asia/Tokyo",
+			slug: "tokyo-hamarikyuu-gardens-man",
+		},
+		{
+			src: "20240420-3_87A1697-Enhanced-NR.jpg",
+			timezone: "Asia/Tokyo",
+			slug: "tokyo-skytree-ueno-park",
+		},
+		{
+			src: "20240425-3_87A2785-Pano.jpg",
+			timezone: "Asia/Tokyo",
+			slug: "tokyo-hamarikyuu-gardens-pano",
+		},
+		{
+			src: "20240426-3_87A3051-Enhanced-NR.jpg",
+			timezone: "Asia/Tokyo",
+			slug: "tokyo-kanda-river",
+		},
+		{
+			src: "20240426-3_87A3076-Enhanced-NR.jpg",
+			timezone: "Asia/Tokyo",
+			slug: "tokyo-walking-sumida",
+			noLicense: true,
+		},
+		{
+			src: "20240408-0_87A9513-Enhanced-NR.jpg",
+			timezone: "Asia/Tokyo",
+			slug: "tokyo-row-blossoms",
+			noLicense: true,
+		},
+	],
+};
+
 /**
  * @name ALBUMS
  * @description Array of all albums to display, order matters.
@@ -264,6 +322,7 @@ export const ALBUMS: Album[] = [
 	IzuPeninsula,
 	TokyoSkytreeVistas,
 	Kyoto,
+	TokyoDay,
 ];
 
 /**
